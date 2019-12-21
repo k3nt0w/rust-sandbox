bbde7ffa491f7df49d959201473c3098882d9558
k3nt0w <kento.watanabe.11@gmail.com>
2019-12-21 23:38:23 +0900

    feat: main function

--- socket/src/main.rs
+++ socket/src/main.rs
@@ -1,3 +1,47 @@
+use std::env;
+
+#[macro_use]
+
+extern crate log;
+
+mod tcp_client;
+mod tcp_server;
+mod udp_client;
+mod udp_server;
+
 fn main() {
-    println!("Hello, world!");
+    env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 4 {
+        error!("Please specify [tcp|udp] [server|client] [addr:port].");
+        std::process::exit(1);
+    }
+
+    let protocol: &str = &args[1];
+    let role: &str = &args[2];
+    let address = &args[3];
+
+    match protocol {
+        "tcp" => match role {
+            "server" => println!("tcp server"),
+            "client" => println!("tcp client"),
+            _ => missing_role(),
+        },
+        "udp" => match role {
+            "server" => println!("udp server"),
+            "client" => println!("udp client"),
+            _ => missing_role(),
+        },
+        _ => {
+            error!("Please specify tcp or udp on the 1st argument.");
+            std::process::exit(1);
+        }
+    }
+}
+
+fn missing_role() {
+    error!("Please specify server or client on the 2nd argumnet.");
+    std::process::exit(1);
 }
